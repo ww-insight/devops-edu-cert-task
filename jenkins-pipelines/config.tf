3f22d4477a7505b854c2dd1132c44cd66d34d334
wwbel <ww.bel@ya.ru>
2022-07-13 10:57:18 +0300

    apt install python3-pip

--- jenkins-pipelines/config.tf
+++ jenkins-pipelines/config.tf
@@ -68,6 +68,8 @@ resource "yandex_compute_instance" "devops-builder" {
     inline = [
        "sudo apt update"
       ,"sudo apt install python3 -y"
+      ,"sudo apt install python3-pip -y"
+      ,"pip install docker"
     ]
   }
 }
